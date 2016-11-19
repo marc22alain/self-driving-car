ca850529a0569b098165fcef355a0db5652c7e89
marc22alain <marc@marcotypes.ca>
2016-11-19 14:31:25 -0500

    Revision to reflect actual range of motion for steering servo
    There is a physical restriction preventing motion within the 0 - 19
    degree range.

--- arduino/servo_centering/servo_centering.ino
+++ arduino/servo_centering/servo_centering.ino
@@ -15,6 +15,10 @@ Servo steering_servo;  // create servo object to control a servo
 Servo ranging_servo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
+/*
+ * NB: the steering servo is physically range-restricted
+ *     to 20 - 180 degrees
+ */
 int pos = 0;    // variable to store the servo position
 
 
@@ -24,7 +28,7 @@ void setup() {
   steering_servo.write(90);              // tell servo to go to centered position
   ranging_servo.write(90);              // tell servo to go to centered position
   delay(10000);
-    for (pos = 90; pos >= 0; pos -= 1) { // goes from centered position to 0 degrees
+    for (pos = 90; pos >= 20; pos -= 1) { // goes from centered position to 0 degrees
     steering_servo.write(pos);              // tell servo to go to position in variable 'pos'
     ranging_servo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(5);                       // waits 15ms for the servo to reach the position
@@ -33,12 +37,12 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 20; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     steering_servo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = 180; pos >= 20; pos -= 1) { // goes from 180 degrees to 0 degrees
     steering_servo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
